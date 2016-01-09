dd0d6894db51b8f580d57250a6bdc3c85bf278d4
tehran980 <hsn.m@torkmail.ir>
2016-01-09 00:16:35 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {93367845,128729099,0,tonumber(our_id)},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
